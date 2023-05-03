37b353377ab011709e28d5b57ec7c5d8afb774f2
Ryoichi <122389227+glitchxxx0@users.noreply.github.com>
2023-05-03 20:22:58 +0530

    Add files via upload

---
+++ C++ L01/firstprogram.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"your first program"<<endl;
+}
---
+++ C++ L01/typecasting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //implicit typecasting
+    int a='a'; 
+    cout<<a<<endl;
+    //explicit typecasting
+    int b= int(5.002); 
+    cout<<b<<endl;
+}
